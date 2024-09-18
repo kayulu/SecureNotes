40d28eb1a68c0243e4bba9a8fb111220a19611d0
Kayhan <Kayhan@ulu.de>
2024-09-18 23:28:24 +0200

    Add Spring Security and basic authentication

--- src/main/java/com/kayulu/securenotes/SecureNotesApplication.java
+++ src/main/java/com/kayulu/securenotes/SecureNotesApplication.java
@@ -2,6 +2,7 @@ package com.kayulu.securenotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @SpringBootApplication
 public class SecureNotesApplication {
@@ -9,5 +10,4 @@ public class SecureNotesApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecureNotesApplication.class, args);
     }
-
 }
